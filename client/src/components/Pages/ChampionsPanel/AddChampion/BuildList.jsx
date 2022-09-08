166f2df3f8dbd7895a080895c45c4b3120eb5cd8
TheBlackIgor <swierczynskigor@gmail.com>
2022-09-09 00:37:18 +0200

    próbowałem

--- client/src/components/Pages/ChampionsPanel/AddChampion/BuildList.jsx
+++ client/src/components/Pages/ChampionsPanel/AddChampion/BuildList.jsx
@@ -5,14 +5,70 @@ import "./BuildList.css";
 export default function BuildList(props) {
   console.log(props.builds);
   const builds = props.builds.map(build => {
+    const starters = build.starters.map(starter => {
+      if (starter !== "")
+        return (
+          <div className="item-container">
+            <img
+              src={"./images/items/" + starter.image}
+              alt={starter.image}
+            ></img>
+          </div>
+        )
+      else return null
+    })
+
+    const buildComp = build.build.map(bd => {
+      return (
+        <div className="item-container">
+          <img
+            src={"./images/items/" + bd.image}
+            alt={bd.image}
+          ></img>
+        </div>
+      )
+    })
+
+    const mainRunes = build.firtstRunes.map(rune => {
+      return (
+        <div className="item-container">
+          <img
+            src={"./images/" + rune.image}
+            alt={rune.image}
+          ></img>
+        </div>
+      )
+    })
+    const secondRunes = build.secondRunes.map(rune => {
+      return (
+        <div className="item-container">
+          <img
+            src={"./images/" + rune.image}
+            alt={rune.image}
+          ></img>
+        </div>
+      )
+    })
+
     return (
       <div>
-
+        <div>
+          {starters}
+        </div>
+        <div>
+          {buildComp}
+        </div>
+        <div>
+          {mainRunes}
+        </div>
+        <div>
+          {secondRunes}
+        </div>
       </div>
     )
   })
   if (props.builds)
     return <div className="list">
-
+      {builds}
     </div>;
 }
