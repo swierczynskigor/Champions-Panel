46ba1be041ea403850bb97b5812fbf024e871295
TheBlackIgor <swierczynskigor@gmail.com>
2022-09-13 19:45:01 +0200

    build list update

--- client/src/components/Pages/ChampionsPanel/AddChampion/BuildList.jsx
+++ client/src/components/Pages/ChampionsPanel/AddChampion/BuildList.jsx
@@ -4,71 +4,65 @@ import "./BuildList.css";
 
 export default function BuildList(props) {
   console.log(props.builds);
-  const builds = props.builds.map(build => {
-    const starters = build.starters.map(starter => {
+  let idx = -1;
+
+  const handleDeleteBuild = (index) => {};
+
+  const builds = props.builds.map((build) => {
+    idx++;
+    let key = Math.random();
+    const starters = build.starters.map((starter) => {
+      key++;
       if (starter !== "")
         return (
-          <div className="item-container">
+          <div className="item-container" key={key}>
             <img
               src={"./images/items/" + starter.image}
               alt={starter.image}
             ></img>
           </div>
-        )
-      else return null
-    })
+        );
+      else return null;
+    });
 
-    const buildComp = build.build.map(bd => {
+    const buildComp = build.build.map((bd) => {
+      key++;
       return (
-        <div className="item-container">
-          <img
-            src={"./images/items/" + bd.image}
-            alt={bd.image}
-          ></img>
+        <div className="item-container" key={key}>
+          <img src={"./images/items/" + bd.image} alt={bd.image}></img>
         </div>
-      )
-    })
+      );
+    });
 
-    const mainRunes = build.firtstRunes.map(rune => {
+    const mainRunes = build.firstRunes.map((rune) => {
+      key++;
       return (
-        <div className="item-container">
-          <img
-            src={"./images/" + rune.image}
-            alt={rune.image}
-          ></img>
+        <div className="item-container" key={key}>
+          <img src={"./images/runes/" + rune.image} alt={rune.image}></img>
         </div>
-      )
-    })
-    const secondRunes = build.secondRunes.map(rune => {
+      );
+    });
+    const secondRunes = build.secondRunes.map((rune) => {
+      key++;
       return (
-        <div className="item-container">
-          <img
-            src={"./images/" + rune.image}
-            alt={rune.image}
-          ></img>
+        <div className="item-container" key={key}>
+          <img src={"./images/runes/" + rune.image} alt={rune.image}></img>
         </div>
-      )
-    })
+      );
+    });
 
     return (
-      <div>
-        <div>
-          {starters}
-        </div>
-        <div>
-          {buildComp}
-        </div>
-        <div>
-          {mainRunes}
-        </div>
-        <div>
-          {secondRunes}
+      <div key={Math.random()} className="build">
+        <div>{starters}</div>
+        <div className="main-build">{buildComp}</div>
+        <div>{mainRunes}</div>
+        <div>{secondRunes}</div>
+        <div className="btn">
+          <button onClick={() => handleDeleteBuild(idx)}>Delete</button>
         </div>
       </div>
-    )
-  })
-  if (props.builds)
-    return <div className="list">
-      {builds}
-    </div>;
+    );
+  });
+  if (props.builds) return <div className="list">{builds}</div>;
+  else return null;
 }
