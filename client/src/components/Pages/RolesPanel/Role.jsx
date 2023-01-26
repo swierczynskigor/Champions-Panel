8dbbad217a056a5e658a6c8816bf50f31c60bf7e
Igor Świerczyński <swierczynskigor@gmail.com>
2023-01-26 12:11:50 +0100

    viewing list

--- client/src/components/Pages/RolesPanel/Role.jsx
+++ client/src/components/Pages/RolesPanel/Role.jsx
@@ -1,20 +1,55 @@
 import React, { useState } from "react";
 import Button from "../../Ui/Button";
 import ChampionPicker from "../../Ui/ChampionPicker/ChampionPicker";
+import { useSelector } from "react-redux";
+import Miniature from "../../Ui/Miniature";
 
 import "./Role.css";
 
 export default function Role(props) {
   const [showChampionPicker, setShowChampionPicker] = useState(false);
+  const [showSelectBuildsModal, setShowSelectBuildsModal] = useState(false);
+  const championsInRole = useSelector((state) => state.roles[props.val]);
+  const champions = useSelector((state) => state.champions.champions);
+
+  const [nameOfPickedChamp, setNameOfPickedChamp] = useState("");
+  const [buildsToAssing, setBuildsToAssing] = useState([]);
 
   const changeChampionPanelVisibility = () =>
     showChampionPicker
       ? setShowChampionPicker(false)
       : setShowChampionPicker(true);
 
+  const changeBuildsPanelVisibility = () =>
+    showSelectBuildsModal
+      ? setShowSelectBuildsModal(false)
+      : setShowSelectBuildsModal(true);
+
   const handlePickChampion = () => {
     changeChampionPanelVisibility();
   };
+
+  const handleManageChampionBuild = (pickedChampion) => {
+    changeBuildsPanelVisibility();
+    console.log(pickedChampion);
+    const championsBuilds = champions.find(
+      (champ) => champ.name === pickedChampion.name
+    ).builds;
+    setNameOfPickedChamp(pickedChampion.name);
+    setBuildsToAssing(championsBuilds);
+  };
+
+  const championsList = championsInRole.map((champion) => {
+    return (
+      <Miniature
+        key={champion.name}
+        name={champion.name}
+        type={"champion"}
+        image={champion.image}
+        click={handleManageChampionBuild}
+      />
+    );
+  });
 
   if (props.val !== "duobot")
     return (
@@ -25,13 +60,14 @@ export default function Role(props) {
             role={props.val}
           />
         ) : null}
+        {showSelectBuildsModal ? <></> : null}
         <div className="main-role">
           <nav>
             <Button styles={2} func={handlePickChampion}>
               Select Champions
             </Button>
           </nav>
-          <div></div>
+          <div className="champion-list">{championsList}</div>
         </div>
       </>
     );
---
+++ client/src/components/Pages/RolesPanel/SelectBuilds/SelectBuilds.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SelectBuilds = (props) => {
+  return <div>SelectBuilds</div>;
+};
